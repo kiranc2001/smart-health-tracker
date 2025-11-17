5c415c8115090b7858102291f9ed550774cb5aef
Kiran C <kirangowda0212@gmail.com>
2025-11-17 08:19:31 +0530

    added a case insensitive check for email

--- src/main/java/com/healthtracker/repository/UserRepository.java
+++ src/main/java/com/healthtracker/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.healthtracker.repository;
 
 import com.healthtracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE LOWER(u.email) = LOWER(:email)")
     Optional<User> findByEmail(String email);
 }
